a07ebcf6e2872307953fe0c9afff1acfc01671c0
Rogowiak <73337380+Rogowiak@users.noreply.github.com>
2021-10-25 18:06:12 +0200

    Add files via upload

---
+++ LAB01/ZAD02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ZAD02
+{
+    class Program
+    {
+        static int Liczba()
+        {
+            int liczba = 420;
+            Console.WriteLine(liczba);
+            return liczba;
+        }
+
+        static void Main(string[] args)
+        {
+            Liczba();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LAB01/ZAD03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ZAD03
+{
+    class Program
+    {
+        static double PoleProstokata(double a, double b)
+        {
+            double wynik = a * b;
+            Console.WriteLine(wynik);
+            return wynik;
+        }
+        static void Main(string[] args)
+        {
+            PoleProstokata(21, 2);
+            Console.ReadKey();
+        }
+    }
+}
